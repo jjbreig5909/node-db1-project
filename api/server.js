511f777bee175363be93d483aafc4c0ccc5d5ea6
unknown <jjbreig@gmail.com>
2020-04-08 22:31:42 -0400

    complete

--- api/server.js
+++ api/server.js
@@ -61,6 +61,17 @@ server.put('/:id', (req, res)=>{
     });
 });
 
+server.delete('/:id', async (req, res)=>{
+    const{id} = req.params;
+    try {
+        const count=await db.del().from('accounts').where({id});
+        count ? res.status(200).json({deleted: count})
+            : res.status(404).json({message:'invalid id'});
+    } catch (err) {
+        res.status(500).json({message:"DB Issue", error: err});
+    }
+});
+
 
 
 
